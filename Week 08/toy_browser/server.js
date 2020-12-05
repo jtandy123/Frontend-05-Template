c0b5e451e72260e40b1229c7fc9deb5912b01351
Andy Jiang <982071036@qq.com>
2020-12-05 21:31:22 +0800

    update parse HTML

--- Week 08/toy_browser/server.js
+++ Week 08/toy_browser/server.js
@@ -9,8 +9,31 @@ http.createServer((request, response) => {
   }).on('end', () => {
     // body = Buffer.from(body).toString();
     console.log('body: ', body);
+    response.setHeader('Content-Type', 'text/html');
+    response.setHeader('X-Foo', 'bar');
     response.writeHead(200, {'Content-Type': 'text/html'});
-    response.end(' Hello World\n');
+    // response.end(' Hello World\n');
+    response.end(`
+  <html maaa=a>
+  <head>
+    <style>
+      body div #myid {
+        width: 100px;
+        background-color: #ff5000;
+      }
+      body div img {
+        width: 30px;
+        background-color: #ff1111;
+      }
+    </style>
+  </head>
+  <body>
+    <div>
+      <img id="myid" />
+      <img />
+    </div>
+  </body>
+</html>`);
   });
 }).listen(8088, () => {
   console.log('Server started on port: 8088')
